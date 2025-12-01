7e3e5aa36a85c0a4f76573b260a289197ce0b639
itsscb <dev@itsscb.de>
2025-12-01 23:53:42 +0100

    only run if input exits

--- 2025/src/main.rs
+++ 2025/src/main.rs
@@ -3,9 +3,10 @@ use std::{fs::File, io::Read};
 use aoc25::one::solve_one;
 
 fn main() {
-    let mut input = File::open("inputs/one.txt").unwrap();
-    let mut buf: Vec<u8> = Vec::with_capacity(10_000);
-    input.read_to_end(&mut buf).unwrap();
-    let result = solve_one(&buf);
-    println!("Result One: {result}");
+    if let Ok(mut input) = File::open("inputs/one.txt") {
+        let mut buf: Vec<u8> = Vec::with_capacity(10_000);
+        input.read_to_end(&mut buf).unwrap();
+        let result = solve_one(&buf);
+        println!("Result One: {result}");
+    }
 }
