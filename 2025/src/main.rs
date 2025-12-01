56c2c31ccb43ce890fafe9d2c3916ee1810e6183
itsscb <dev@itsscb.de>
2025-12-02 00:55:56 +0100

    two - with bugs

--- 2025/src/main.rs
+++ 2025/src/main.rs
@@ -1,12 +1,15 @@
 use std::{fs::File, io::Read};
 
-use aoc25::one::solve_one;
+use aoc25::{one::solve_one, two::solve_two};
 
 fn main() {
     if let Ok(mut input) = File::open("inputs/one.txt") {
         let mut buf: Vec<u8> = Vec::with_capacity(10_000);
         input.read_to_end(&mut buf).unwrap();
         let result = solve_one(&buf);
+        println!("Result One: {result}");
+
+        let result = solve_two(&buf);
         println!("Result One: {result}");
     }
 }
---
+++ 2025/src/two.rs
@@ -0,0 +1,131 @@
+#[cfg(test)]
+mod test {
+    pub use super::*;
+
+    #[test]
+    fn test_safe() {
+        let mut safe = Safe::default();
+        assert_eq!(safe.current, 50);
+        safe.rotate(Rotation::R(1000));
+        assert_eq!(safe.current, 50);
+        assert_eq!(safe.get_counter(), 10);
+        let mut safe = Safe::default();
+        assert_eq!(safe.current, 50);
+
+        safe.rotate(Rotation::L(68));
+        assert_eq!(safe.current, 82);
+        assert_eq!(safe.get_counter(), 1);
+        safe.rotate(Rotation::L(30));
+        assert_eq!(safe.current, 52);
+        safe.rotate(Rotation::R(48));
+        assert_eq!(safe.current, 0);
+        assert_eq!(safe.get_counter(), 2);
+        safe.rotate(Rotation::L(5));
+        assert_eq!(safe.current, 95);
+        safe.rotate(Rotation::R(60));
+        assert_eq!(safe.current, 55);
+        assert_eq!(safe.get_counter(), 3);
+        safe.rotate(Rotation::L(55));
+        assert_eq!(safe.current, 0);
+        assert_eq!(safe.get_counter(), 4);
+
+        // TODO: This shouldn't fail!
+        safe.rotate(Rotation::L(1));
+        assert_eq!(safe.current, 99);
+        assert_eq!(safe.get_counter(), 4);
+
+        safe.rotate(Rotation::L(99));
+        assert_eq!(safe.current, 0);
+        assert_eq!(safe.get_counter(), 5);
+        safe.rotate(Rotation::R(14));
+        assert_eq!(safe.current, 14);
+        safe.rotate(Rotation::L(82));
+        assert_eq!(safe.current, 32);
+        assert_eq!(safe.get_counter(), 6);
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Safe {
+    current: u32,
+    counter: u32,
+}
+
+impl Default for Safe {
+    fn default() -> Self {
+        Self {
+            current: 50,
+            counter: Default::default(),
+        }
+    }
+}
+
+impl Safe {
+    fn right(&mut self, count: u32) {
+        self.current = (self.current + count) % 100;
+        self.counter += count / 100;
+        if self.current == 0 {
+            self.counter += 1;
+        }
+    }
+
+    fn left(&mut self, count: u32) {
+        let num = count % 100;
+        if num <= self.current {
+            self.current -= num;
+        } else {
+            self.counter += 1;
+            self.current = 100 - (num - self.current);
+        }
+        if self.current == 0 {
+            self.counter += 1;
+        }
+
+        self.counter += count / 100;
+    }
+
+    pub fn rotate(&mut self, rotation: Rotation) {
+        match rotation {
+            Rotation::L(r) => self.left(r),
+
+            Rotation::R(r) => self.right(r),
+        }
+        // if self.current == 0 {
+        //     self.counter += 1;
+        // }
+    }
+
+    pub fn get_counter(&self) -> u32 {
+        self.counter
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum Rotation {
+    L(u32),
+    R(u32),
+}
+
+impl From<&[u8]> for Rotation {
+    fn from(value: &[u8]) -> Self {
+        let num: &[u8] = &value[1..];
+        // SAFETY: Promised by input
+        let num: u32 = std::str::from_utf8(num).unwrap().parse::<u32>().unwrap();
+        let res = match value[0] {
+            b'L' => Rotation::L(num),
+            b'R' => Rotation::R(num),
+            _ => unreachable!("should never happen"),
+        };
+        res
+    }
+}
+
+pub fn solve_two(input: &[u8]) -> u32 {
+    let mut safe = Safe::default();
+    input
+        .split(|c| *c == b'\n')
+        .filter(|line| !line.is_empty())
+        .map(|line| Rotation::from(line))
+        .for_each(|rotation| safe.rotate(rotation));
+    safe.counter
+}
