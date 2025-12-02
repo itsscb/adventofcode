ebf1387fa3d6342e5f8664ad71d9b257b6c5f47c
itsscb <dev@itsscb.de>
2025-12-02 11:07:04 +0100

    three

--- 2025/src/main.rs
+++ 2025/src/main.rs
@@ -1,6 +1,6 @@
 use std::{fs::File, io::Read};
 
-use aoc25::{one::solve_one, two::solve_two};
+use aoc25::{one::solve_one, three::solve_three, two::solve_two};
 
 fn main() {
     if let Ok(mut input) = File::open("inputs/one.txt") {
@@ -10,6 +10,12 @@ fn main() {
         println!("Result One: {result}");
 
         let result = solve_two(&buf);
-        println!("Result One: {result}");
+        println!("Result Two: {result}");
+    }
+    if let Ok(mut input) = File::open("inputs/three.txt") {
+        let mut buf: Vec<u8> = Vec::with_capacity(10_000);
+        input.read_to_end(&mut buf).unwrap();
+        let result = solve_three(&buf);
+        println!("Result Three: {result}");
     }
 }
