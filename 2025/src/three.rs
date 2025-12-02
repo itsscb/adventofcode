818d1c48534f19667ee44764d1ae42ba2bbe3884
itsscb <dev@itsscb.de>
2025-12-02 12:13:28 +0100

    four

---
+++ 2025/src/three.rs
@@ -0,0 +1,125 @@
+#[derive(Debug, Clone, Default)]
+struct Range {
+    start: u64,
+    end: u64,
+}
+
+impl Range {
+    fn find_invalid(&self) -> Vec<u64> {
+        (self.start..self.end)
+            .filter(|&i| {
+                let s = format!("{}", i);
+                let first = &s[0..s.len() / 2];
+                let second = &s[s.len() / 2..];
+                first == second
+            })
+            .collect()
+    }
+
+    fn find_invalid_thorough(&self) -> Vec<u64> {
+        (self.start..self.end)
+            .filter(|&i| {
+                let s = format!("{}", i);
+                check_id(&s)
+            })
+            .collect()
+    }
+}
+
+fn check_id(id: &str) -> bool {
+    let mut window = 1;
+    while window < id.len() {
+        let mut chunks = id.as_bytes().chunks_exact(window);
+        let first = chunks.next().unwrap();
+
+        if chunks.all(|c| c == first) && chunks.remainder().is_empty() {
+            return true;
+        }
+
+        window += 1;
+    }
+    return false;
+}
+
+impl<T: AsRef<[u8]>> From<T> for Range {
+    fn from(value: T) -> Self {
+        let v: Vec<_> = value
+            .as_ref()
+            .split(|c| *c == b'-')
+            // SAFETY: We know that the input is valid UTF-8.
+            .map(|s| unsafe { std::str::from_utf8_unchecked(s) })
+            .collect();
+        let start: u64 = v[0].parse().unwrap();
+        let end: u64 = v[1].replace("\r", "").replace("\n", "").parse().unwrap();
+        Self {
+            start,
+            end,
+            ..Default::default()
+        }
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+struct Ranges {
+    ranges: Vec<Range>,
+}
+
+impl Ranges {
+    fn find_invalid_ids(&self) -> Vec<u64> {
+        self.ranges.iter().flat_map(|r| r.find_invalid()).collect()
+    }
+    fn find_invalid_ids_thorough(&self) -> Vec<u64> {
+        self.ranges
+            .iter()
+            .flat_map(|r| r.find_invalid_thorough())
+            .collect()
+    }
+
+    fn sum_invalid_ids_thorough(&self) -> u64 {
+        self.find_invalid_ids_thorough().iter().sum()
+    }
+
+    fn sum_invalid_ids(&self) -> u64 {
+        self.find_invalid_ids().iter().sum()
+    }
+}
+
+impl<T: AsRef<[u8]>> From<T> for Ranges {
+    fn from(value: T) -> Self {
+        Self {
+            ranges: value
+                .as_ref()
+                .split(|c| *c == b',')
+                .map(Range::from)
+                .collect(),
+            ..Default::default()
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_range() {
+        let range = Range::from(b"1-3");
+        assert_eq!(range.start, 1);
+        let range = Range::from(b"1-13");
+        assert_eq!(range.start, 1);
+        assert_eq!(range.end, 13);
+        assert_eq!(range.find_invalid(), vec![11u64]);
+    }
+
+    #[test]
+    fn test_check_id() {
+        assert!(check_id("95-115"));
+    }
+}
+
+pub fn solve_three(input: &[u8]) -> u64 {
+    Ranges::from(input).sum_invalid_ids()
+}
+pub fn solve_four(input: &[u8]) -> u64 {
+    Ranges::from(input).sum_invalid_ids_thorough()
+}
