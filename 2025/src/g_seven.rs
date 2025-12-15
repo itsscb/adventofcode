3a022ed2e4c1c6f504bc65d16f2729e8150b88c6
itsscb <51835292+itsscb@users.noreply.github.com>
2025-12-15 23:10:54 +0100

    fix: logic bug. reminder: learn to read

--- 2025/src/g_seven.rs
+++ 2025/src/g_seven.rs
@@ -1,28 +1,38 @@
+const MATCHES: [(isize, isize); 8] = [
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+];
+
 #[allow(clippy::suspicious_map)]
 #[must_use]
 pub fn solve_seven(input: &[u8]) -> usize {
-    // SAFETY: input is valid UTF-8
-    let input = unsafe { std::str::from_utf8_unchecked(input) };
-    input
-        .lines()
-        .map(|line| {
-            line.chars()
-                .enumerate()
-                .filter(|(i, c)| {
-                    let skip = i.saturating_sub(5);
-                    let take = if skip <= 5 { 8 - skip } else { 8 };
-                    let m = line
-                        .chars()
-                        .skip(skip)
-                        .take(take)
-                        .filter(|c| *c == '@')
-                        .count()
-                        .saturating_sub(1);
+    let lines: Vec<_> = input.split(|b| b == &b'\n').collect();
 
-                    println!("{i} {c} {m}");
-                    *c == '@' && m < 4
-                })
-                .count()
+    (0..lines[0].len())
+        .flat_map(|x| (0..lines.len()).map(move |y| (x, y)))
+        .filter(|&(x, y)| {
+            if let Some(y1) = lines.get(y) {
+                if let Some(x1) = y1.get(x) {
+                    return x1 == &b'@'
+                        && MATCHES
+                            .iter()
+                            .filter(|&(x2, y2)| {
+                                lines
+                                    .get(y.wrapping_add_signed(*y2))
+                                    .and_then(|r| r.get(x.wrapping_add_signed(*x2)))
+                                    .is_some_and(|&c| c == b'@')
+                            })
+                            .count()
+                            < 4;
+                }
+            }
+            false
         })
         .count()
 }
